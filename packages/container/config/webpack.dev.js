5383e38d56cc3845adf17eb6b1fb1c7168c66135
jhonnyalphas <jhonny.arana@alphas.technology>
2021-08-30 11:27:54 -0500

    corrige boton

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,30 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { container } = require('webpack');
+const packagesJson = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name:'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+      },
+      shared: packagesJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
+
+module.exports = merge(commonConfig, devConfig);
